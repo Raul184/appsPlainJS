3bb75aa39423da7c6faaf080748bd02cc5ef32e5
RrChaos <rrchaos@tutanota.com>
2018-11-13 18:06:53 +0000

    update on PigGame

--- pigGame/assets/app/app.js
+++ pigGame/assets/app/app.js
@@ -1,31 +1,8 @@
 console.log('works?');
-// ALL BUTTONS
-const handlers = document.getElementsByClassName('tablinks');
-//ALL CONTENT TO BE DISPLAYED
-const tabcontent = document.getElementsByClassName("tabcontent");
-//EVENTLISTENERS
-for (let i = 0;  i < handlers.length; i++){
-  //make sure no Active class is populated on any other element when you run
-  handlers[i].addEventListener('click', () => {
-    for (let i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].className = tabcontent[i].className.replace(" active", "");
-    }
-    // add Active class just on selected element
-    let result = handlers[i].innerHTML;
-    let comparator = document.getElementById(result);
-    comparator.classList.toggle('active');
-  });
-}
-// ANIMATED TABS END
-//---------------------------------------------------------------------
-const controller = document.getElementsByClassName("tablinkI");
-for (let i = 0 ; i < controller.length; i++){
-   controller[i].addEventListener('click', () =>{
-     const result = document.getElementsByClassName("tabcontentI");
-     for (let i = 0 ; i <result.length; i++){
-        result[i].className = result[i].className.replace(" activeI", "");
-     }
-      document.getElementById(controller[i].innerHTML).classList.toggle('activeI');
-   })
-}
+// GAME RULES:
+ // - The game has 2 players playing by rounds.
+// - Every player can roll a dice as many time as he/she wants and each result will be stored in his/her Round score
+// - If any player rolls a 1 during his/her round , all the score gotten in the round will be lost and next player will start a new round
+// - First player on reach 100 score wins the game
+   
 //------------------------------------------------------------------
