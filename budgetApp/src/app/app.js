c4347d258568e827270e9b7d43947e97dd6584d5
RrChaos <rrchaos@tutanota.com>
2018-11-15 14:29:35 +0000

    Plain JS being applied to bring full functionality to my Budget desktop app

--- budgetApp/src/app/app.js
+++ budgetApp/src/app/app.js
@@ -1,1 +1,50 @@
 console.log('works?');
+//Budget App
+// TASKS LIST
+// - Event handlers
+// - Get input
+// - Add input into calcule structure
+// Add input to UI
+// Calculate budget
+// Update UI
+//--------------
+// ARQUITECTURE
+// Module 1: UI Module------------------
+// - Get input
+// Add input to UI
+// Update UI
+// Module 2: Data Module----------------
+// - Add input into calcule structure
+// Calculate budget
+// Module 3: CONTROLLER-----------
+// - Event handlers
+//---------------------------------------------------------------------
+// GLOBAL SCOPE
+
+// MODULE 1: DATA MODULE
+const budgetController = function(){
+    const x = 23;
+    const add = function(a){
+      return x + a;
+    }
+    return {
+      publicTest: function(b){
+        return add(b);
+      }
+    }
+}();
+
+// MODULE 2: UI MODULE
+const uiController = function(){
+
+}();
+
+// BRIDGE BETWEEN OUR DATA MODULE & UI MODULE ABOVE
+const controller =  function(budget, ui){
+    let secondTest = budget.publicTest(5);
+    return{
+      anotherPublicTest: function(){
+        console.log(secondTest);
+      }
+    }
+}(budgetController, uiController);
