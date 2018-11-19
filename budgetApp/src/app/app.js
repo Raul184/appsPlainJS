84dda8846f18bbe6aa420ff6d42ee50481bbe00c
RrChaos <rrchaos@tutanota.com>
2018-11-19 18:45:27 +0000

    Fully Budget desktop App with JS

--- budgetApp/src/app/app.js
+++ budgetApp/src/app/app.js
@@ -14,7 +14,6 @@ console.log('works?');
 // GLOBAL SCOPE
 // MODULE 1: DATA MODULE
 const budgetController = function(){
-
       const Expense = function(id, description, value){
           this.id = id;
           this.description = description;
@@ -37,42 +36,53 @@ const budgetController = function(){
           },
           budget: 0,
           percentage: 0
-      }
+      };
       return{
         //1 ADD into DATABASE method
         addItem: function (type, des, val){
             var item, iD;
-            //1.  iD assignator
+                                  //1.  iD assignator
             if(data.allItems[type].length > 0 ){
             iD = data.allItems[type][data.allItems[type].length -1].id + 1;
           }else{ iD = 0} //when there's no Items , Id in Stock = 0;
-            //2.   Stock Classifier
+                                  //2.   Stock Classifier
             if (type === 'exp'){ item = new Expense(iD , des, val); }
             else if (type === 'inc'){ item = new Income(iD , des, val); }
-            //and then push it to Storage
+                                          //and then push it to Storage
             data.allItems[type].push(item);
             return item;
         },
-        //2 CALCULATE Expenses from DATABASE method
+        // 2 REMOVE selected ITEM
+        removeItem: function(type, id){
+          let ids, locator;
+            //check Ids and find the selected one
+            ids = data.allItems[type].map(function(current) {return current.id});
+            locator = ids.indexOf(id);
+            // remove selected id (item)
+            if(locator !== -1){
+                data.allItems[type].splice(locator, 1);
+                }
+        },
+        //3 CALCULATE Expenses from DATABASE method
         calculatorExp: function(){
             let expenses;
             expenses = data.allItems.exp.map(exP => { return exP.value});
             data.totals.exp = expenses.reduce((acc, value) => {return acc += value}, 0);
             return data.totals.exp;
         },
-        //3 CALCULATE Income from DATABASE method
+        //4 CALCULATE Income from DATABASE method
         calculatorInc: function(){
             let income;
             income = data.allItems.inc.map(inC => { return inC.value});
             data.totals.inc = income.reduce((acc, value) => { return acc += value}, 0);
             return data.totals.inc;
         },
-        // 4 CALCULATE TOTALS
+        // 5 CALCULATE TOTALS
         calculatorTotal: function(){
             data.budget = data.totals.inc - data.totals.exp;
             return data.budget;
         },
-        // 5 PERCENTAGE
+        // 6 PERCENTAGE
         percentageCalculator: function(){
             if(data.totals.inc > 0 ){
                 data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
@@ -116,12 +126,12 @@ const uiController = function () {
                 // 2.1  HTML string
                   if( type === 'inc'){
                       element = domStrings.incomeList;
-                      html = `<div class=' item ' id=' income-%id% '><div class=' item__description blue '>%description%</div>
+                      html = `<div class=' item ' id='inc-%id%'><div class=' item__description blue '>%description%</div>
                       <div class=' item__value blue '>%value%</div><button class="item__delete--btn blue"><i class="ion-ios-close-outline">
                       </i></button></div>` ;
                     } else if( type === 'exp'){
                       element = domStrings.expensesList;
-                      html = `<div class="item" id="expense-%id%"><div class=" item__description">%description%</div>
+                      html = `<div class="item" id="exp-%id%"><div class=" item__description">%description%</div>
                       <div class="item__value">%value%</div><div class="item__percentage">21%</div><button class="item__delete--btn red">
                       <i class="ion-ios-close-outline"></i></button></div>` ;
                     }
@@ -164,6 +174,7 @@ const uiController = function () {
 
 // BRIDGE BETWEEN              DATA  & UI  MODULES ABOVE
 const globalController =  function(budget, ui){
+  // 1
           // Add Input from UI
           const addItem = function(){
               let input, inputInStock;
@@ -180,6 +191,21 @@ const globalController =  function(budget, ui){
               //5 Update UI with Inc & Budget totals at TOP section
               ui.updateBudget(budget.calculatorInc(), budget.calculatorExp(), budget.calculatorTotal(), budget.percentageCalculator());
               }
+        };
+// 2
+        const deleteItem = function(e){
+            let itemSelected, splitId, typeId, itemId;
+            // raw - input
+            itemSelected = e.target.parentNode.id;   //id item logged when button is clicked
+            splitId = itemSelected.split('-');
+            typeItem = splitId[0];  //for the type
+            itemId = parseInt(splitId[1]);   // for the id of item within type
+            // 1 delete Item from dataBase
+            budget.removeItem(typeItem, itemId);
+            // 2 delete from UI
+            document.querySelector(`#${typeItem}-${itemId}`).style.display = 'none';
+            // 3 update & show new budget
+            ui.updateBudget(budget.calculatorInc(), budget.calculatorExp(), budget.calculatorTotal(), budget.percentageCalculator());
         }
           return{
               init: function(){
@@ -194,7 +220,8 @@ const globalController =  function(budget, ui){
                           addItem();
                       }
                   });
-                  // 2.
+                  // 2. Event delegation
+                  document.querySelector('.container').addEventListener('click', deleteItem);
             } //init ENDS
           }
 
