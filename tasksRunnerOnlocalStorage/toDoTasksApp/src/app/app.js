6a8fd1a418093060a6e864e276e375f411dcae69
RrChaos <rrchaos@tutanota.com>
2018-11-22 00:19:25 +0000

    toDoTasksApp on Plain JS and using localStorage API

--- tasksRunnerOnlocalStorage/toDoTasksApp/src/app/app.js
+++ tasksRunnerOnlocalStorage/toDoTasksApp/src/app/app.js
@@ -1,1 +1,11 @@
 console.log('works?');
+// UI
+const init = function() {
+   const domStrings = {
+        form : document.querySelector('#task-form'), //form
+        taskList : document.querySelector('.collection'),
+        clearBtn : document.querySelector('.clear-tasks'), //filter btn
+        filter : document.querySelector('#filter'), //input for filter
+        taskInput : document.querySelector('#task') //add/subm task btn
+   }
+}
