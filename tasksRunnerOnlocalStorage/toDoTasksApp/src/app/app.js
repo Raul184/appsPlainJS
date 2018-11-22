02ab04b3f7d1d3902c2a643bd1c12d5dc068698a
RrChaos <rrchaos@tutanota.com>
2018-11-22 18:36:15 +0000

    toDoTasksApp on Plain JS and using localStorage API

--- tasksRunnerOnlocalStorage/toDoTasksApp/src/app/app.js
+++ tasksRunnerOnlocalStorage/toDoTasksApp/src/app/app.js
@@ -12,32 +12,54 @@ const datab = function(){
               }
               tasks.push(b); //storage them in array...
               localStorage.setItem('tasks', JSON.stringify(tasks));//..as strings
-          }
+          },
 // 1. GET Tasks storaged in localStorage API
           getTasks: function() {
-            let tasks;
-            if(localStorage.getItem('tasks') === null){//empty
-                tasks = []; //prepare to stock in an EmptyArray
-            } else{   //there are stuff, stock them
-              tasks = JSON.parse(localStorage.getItem('tasks'));
-            }
-            tasks.forEach(function(current){ //Check for whatever left task and put them back
-              //Create a li element
-              const li = document.createElement('li');
-              //Add a class
-              li.classList.add('collection-item');
-              //Create text and append
-              li.appendChild(document.createTextNode(domStrings.taskInput.value));
-              //Create delete-link element
-              const link = document.createElement('a');
-              // Add class
-              link.classList.add('delete-item');
-              // Add Icon
-              link.innerHTML = '<i class= "fa fa-remove"></i>';
-              li.appendChild(link);
-              // Now , append Li to ul
-              domStrings.taskList.appendChild(li);
-            });
+              let tasks;
+              if(localStorage.getItem('tasks') === null){//empty
+                  tasks = []; //prepare to stock in an EmptyArray
+              } else{   //there are stuff, stock them
+                  tasks = JSON.parse(localStorage.getItem('tasks'));
+              }
+              tasks.forEach(function(current){ //Check for whateverleft
+                  //Create a li element
+                  const li = document.createElement('li');
+                  //Add a class
+                  li.classList.add('collection-item');
+                  //Create text and append
+                  li.appendChild(document.createTextNode(current));
+                  //Create delete-link element
+                  const link = document.createElement('a');
+                  // Add class
+                  link.classList.add('delete-item');
+                  // Add Icon
+                  link.innerHTML = '<i class= "fa fa-remove"></i>';
+                  li.appendChild(link);
+                  // Now , append Li to ul
+                  document.querySelector('.collection').appendChild(li);
+              });
+          },
+// 2 REMOVE A TASK from localStorage
+          removeTaskFromLocalStorage: function(taskItem){
+              //<li></li> element is the one removed
+              console.log(taskItem);
+              let tasks;
+              // Check Storage
+              if(localStorage.getItem('tasks') === null){//empty
+                  tasks = []; //prepare to stock in an EmptyArray
+              } else{   //there are stuff, stock them
+                  tasks = JSON.parse(localStorage.getItem('tasks'));
+              }
+              tasks.forEach(function(current, index){
+                  if(taskItem.textContent === current){
+                      tasks.splice(index, 1);
+                  }
+              });
+              localStorage.setItem('tasks', JSON.stringify(tasks));
+          },
+// 3 REMOVE ALL TASKS from localStorage
+          clearTasksFromLocalStorage: function() {
+              localStorage.clear();
           }
     }
 }();
@@ -75,7 +97,7 @@ const uiController = function(d){
           li.appendChild(link);
           // Now , append Li to ul
           domStrings.taskList.appendChild(li);
-          // // Once , input is represent in UI , I'll storage so it don't get lost after closing browser
+          // Once , input is represent in UI , I'll storage so it don't get lost after closing browser
           d.storeTaskInLocalStorage(domStrings.taskInput.value);
           // Clear input
           domStrings.taskInput.value = ' ';
@@ -83,7 +105,10 @@ const uiController = function(d){
     },
 // 2 REMOVE 1 input
       removeInput: function(b){
+      //from DOM
           b.target.parentElement.parentElement.remove();
+      //from localStorage
+          d.removeTaskFromLocalStorage(b.target.parentElement.parentElement);
       },
 // 3 REMOVE ALL input
       removeAllInput: function(){
@@ -93,6 +118,7 @@ const uiController = function(d){
           while(domStrings.taskList.firstChild){
               domStrings.taskList.removeChild(domStrings.taskList.firstChild);
           }
+          d.clearTasksFromLocalStorage();
       },
 // 4 FILTER
       tasksFilter: function(b){
@@ -133,7 +159,7 @@ const globalController =  function(ui, db) {
             let dom = ui.getDomStrings();
          // 2 EventListeners
             // localStorage on UI
-            documnet.addEventListener('DOMContentLoaded', tasksGetter);
+            document.addEventListener('DOMContentLoaded', tasksGetter);
             dom.form.addEventListener('submit', addTask); // +  a task
             dom.taskList.addEventListener('click', removeTask);//- 1task
             dom.clearBtn.addEventListener('click', clearAll);// - Altasks
