8ba8f2123cbeb7b83d426a578d30409af8258358
RrChaos <rrchaos@tutanota.com>
2018-11-24 01:03:07 +0000

    A guesser Game on on plain JS

--- smallAppsOnlocalStorage/numberguesser/app.js
+++ smallAppsOnlocalStorage/numberguesser/app.js
@@ -3,63 +3,87 @@
 // - Player gets a certain amount of guesses
 // - Notify player of guesses remaining
 // - Option to play again
-
-// Game values
-let min = 1,
-    max = 10,
-    winningNum = 2,
-    guessesLeft = 3;
-
+const uiController = function(){
+    let scores = {
+      min : 1,
+      max : 10,
+      winningNum : 2,
+      guessesLeft : 3
+    };
 // UI elements
-const game = document.querySelector('#game'),
-       minimum = document.querySelector('.min-num'),
-       maximum = document.querySelector('.max-num'),
-       guessBtn = document.querySelector('#guess-btn'),
-       guessInput = document.querySelector('#guess-input'),
-       message = document.querySelector('.message');
-
-// Assign UI min/max
-minimum.textContent = min;
-maximum.textContent = max;
-
-// EventListeners
-guessBtn.addEventListener('click', function(){
-    let guessedNum = parseInt(guessInput.value); //get a number
-    // validate
-    if( isNaN(guessedNum) || guessedNum < min || guessedNum > max){
-        setMessage(`Please entre a number between ${min} and ${max}`, 'red');
-    }//check if won
-    if(guessedNum === winningNum){
-        gameOver(true, `${winningNum} is correct! , You win!!`);
-    }  else{
-        //substract 1
-        guessesLeft -= 1;
-        if( guessesLeft === 0){
-            //Game over
-          gameOver(false, `Game Over , sorry , you lost. The winning number was: ${winningNum}`);
-        }else {
-            //Game  continues
-            guessInput.style.borderColor = 'red';
-            // clear Input
-            guessInput.value = ' ';
-            // message
-            setMessage(`${guessedNum} is not correct, you still got ${guessesLeft} chances left`, 'red');
-        }
+    let dom = {
+        game : document.querySelector('#game'),
+        minimum : document.querySelector('.min-num'),
+        maximum : document.querySelector('.max-num'),
+        guessBtn : document.querySelector('#guess-btn'),
+        guessInput : document.querySelector('#guess-input'),
+        message : document.querySelector('.message')
+    };
+    dom.minimum.textContent = scores.min;
+    dom.maximum.textContent = scores.max;
+  return {
+      domStrings : function(){
+        return dom;
+      },
+      gameScores : function(){
+        return scores;
+      }
     }
-});
-
-function setMessage(mes, color){
-    message.style.color = color;
-    message.textContent = mes;
+};
+uiController();
+//----------------------------------------------
+const db = function(ui){
+    return{
+      setMessage : function (mes, color){
+          let input = ui.domStrings();
+          input.message.style.color = color;
+          input.message.textContent = mes;
+      },
+      gameOver : function (won, message){
+          let color;
+          won === true ? color = 'green' : 'red';
+          input.guessInput.disabled = true; //disable element if user wins
+          // winner colors
+          input.guessInput.style.borderColor = color;
+          // set text color
+          input.message.style.color = color;
+          setMessage(message);
+      }
+    }
 }
-
-function gameOver(won, message){
-  let color;
-  won === true ? color = 'green' : 'red';
-  guessInput.disabled = true; //disable element if user wins
-  // winner colors
-  guessInput.style.borderColor = color;
-  // set text color
-  message.style.color = color;
-  setMessage(message);
+db(uiController);
+//---------------------------------------------------
+const globalController = function(ui, db){
+  return{
+      init: function(){
+          let input = ui.domStrings();
+          let scores = ui.gameScores();
+          // EventListeners
+          input.guessBtn.addEventListener('click', function(){
+              let guessNum = parseInt(input.guessInput.value); //number
+              // validate
+              if( isNaN(guessNum) || guessNum < scores.min || guessNum > scores.max){
+                  db.setMessage(`Please input a number between ${min} and ${max}`, 'red');
+              }//check if won
+              if(guessNum === scores.winningNum){
+                //Game over
+                  db.gameOver(true, `${winningNum} is correct! , You  win!!`);
+              }else{
+                  //substract 1
+                  scores.guessesLeft -= 1;
+                  if( scores.guessesLeft === 0){
+                      db.gameOver(false, `Game Over , sorry , you lost. The winning number was: ${winningNum}`);
+                  }else {
+                      //Game  continues
+                      input.guessInput.style.borderColor = 'red';
+                      // clear Input
+                      input.guessInput.value = ' ';
+                      // message
+                      db.setMessage(`${guessedNum} is not correct, you still got ${guessesLeft} chances left`, 'red');
+                  }
+              }
+          });
+      }
+  }
 }
+globalController(uiController, db);
