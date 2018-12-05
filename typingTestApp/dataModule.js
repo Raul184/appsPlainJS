1e4b6383ab97b595ac2215a31a074a2c77bad1f3
RrChaos <rrchaos@tutanota.com>
2018-12-05 12:49:03 +0000

    Typing Test App

--- typingTestApp/dataModule.js
+++ typingTestApp/dataModule.js
@@ -1,36 +1,32 @@
-var dataModule = (function(){
-    //character call back used to calculate the number of correct characters inside the current word
-    var nbCorrectChar;
-    var charCallback = function(currentElement, index){
-          nbCorrectChar += (currentElement == this.characters.user[index])? 1 : 0;
-      };
-
-    var appData = {
+const dataModule = (function(){
+// PRIVATE
+    //callback to calculate number of correct chars inside typed-currentWord
+// 1.  DATABASE
+    const appData = {
         indicators: {
-            testStarted: false, testEnded: false, totalTestTime: 0, timeLeft: 0
+            testStarted: false,
+            testEnded: false,
+            totalTestTime: 0,
+            timeLeft: 0
         },
         results: {
-            wpm: 0, wpmChange: 0, cpm: 0, cpmChange: 0, accuracy: 0, accuracyChange: 0 ,   numOfCorrectWords: 0, numOfCorrectCharacters: 0 , numOfTestCharacters: 0
+            wpm: 0, wpmChange: 0, cpm: 0, cpmChange: 0, accuracy: 0, accuracyChange: 0 ,   correctWords: 0, correctChars: 0 , testCharacters: 0
         },
         words: {
-            currentWordIndex: -1, testWords: [], currentWord: {}
+            currentWordIndex: -1,
+            testWords: [],
+            currentWord: {}
         },
     };
-
-    //word constructor
-//    {
-//      value: {correct: '', user: '' , isCorrect: false },
-//      characters: {correct: [], user: [], totalCorrect: 0, totalTest: 0 }
-//    }
-
-    var word = function(index){
-        //word values: correct vs user's
+//2.  WORDS TYPED BY USER ---CONSTRUCTOR
+    let word = function(index){
+//word values: correct vs user's
         this.value = {
             correct: appData.words.testWords[index] + ' ',
             user: '',
             isCorrect: false
         };
-        //characters: correct vs user's
+//characters: correct vs user's
         this.characters = {
             correct: this.value.correct.split(''),
             user: [],
@@ -38,83 +34,75 @@ var dataModule = (function(){
             totalTest: this.value.correct.length
         };
     };
-
-    //update method: updates the word using the word typed by the user
+// 3. CHARS CORRECTOR
+        let nbCorrectChar;
+        let charCallback = function(currentElement, index){
+              nbCorrectChar += (currentElement == this.characters.user[index])? 1 : 0;
+          };
+// 4. JIT STORAGE SYSTEM FOR USER-WORDS RIGHTS & WRONG MISTAKES
+//update method: updates the word using the word typed by the user
     word.prototype.update = function(value){
-
-        //update the user input
+//update the user input
         this.value.user = value;
-
-        //update the words status (correct or not)
+//update the words status (correct or not)
         this.value.isCorrect = (this.value.correct == this.value.user);
-
-        //update user characters
+//update user characters
         this.characters.user = this.value.user.split('');
-
         //calculate the number of correct characters
         //correct: ['w', 'o', 'r', 'd']
         //user: ['w', 'o', 'o', 'w', 'w', 'w', 'w', 'w', 'w', 'w']
         nbCorrectChar = 0;
-
-        var charCallback2 = charCallback.bind(this);
+        let charCallback2 = charCallback.bind(this);
         this.characters.correct.forEach(charCallback2);
 
         this.characters.totalCorrect = nbCorrectChar;
     };
-
     return {
-    //indicators - test Control
-        //sets the total test time to x
+// PUBLIC
+    //INDICATORS
+//sets the total test time to x
         setTestTime: function(x){
             appData.indicators.totalTestTime = x;
         },
-
-        //initializes time left to the total test time
+//initializes time left to the total test time
         initializeTimeLeft: function(){
             appData.indicators.timeLeft = appData.indicators.totalTestTime;
         },
-
-        //starts the test
+//starts the test
         startTest: function(){
             appData.indicators.testStarted = true;
         },
-
+//ends the test
         endTest: function(){
             appData.indicators.testEnded = true;
-        },//ends the test
-
-        //return the remaining test time
+        },
+//return the remaining test time
         getTimeLeft: function(){
             return appData.indicators.timeLeft;
         },
-
-        // reduces the time by one sec
+// reduces the time by one sec
         reduceTime: function(){
             appData.indicators.timeLeft --;
             return appData.indicators.timeLeft;
         },
-
-        //checks if there is time left to continue the test
+//checks if there is time left to continue the test
         timeLeft: function(){
             return appData.indicators.timeLeft != 0;
         },
-
-        //checks if the test has already ended
+//checks if the test has already ended
         testEnded: function(){
             return appData.indicators.testEnded;
         },
-
-        //checks if the test has started
+//checks if the test has started
         testStarted: function(){
             return appData.indicators.testStarted;
         },
 
-    //results
-
-        //calculates wpm and wpmChange and updates them in appData
+    //RESULTS
+//calculates wpm and wpmChange and updates them in appData
         calculateWpm: function(){
-            var wpmOld = appData.results.wpm;
-            var numOfCorrectWords = appData.results.numOfCorrectWords;
+            let wpmOld = appData.results.wpm;
+            let numOfCorrectWords = appData.results.correctWords;
             if(appData.indicators.timeLeft != appData.indicators.totalTestTime){
                 appData.results.wpm = Math.round(60 * numOfCorrectWords/(appData.indicators.totalTestTime - appData.indicators.timeLeft));
             }else{
@@ -124,27 +112,23 @@ var dataModule = (function(){
 
             return [appData.results.wpm, appData.results.wpmChange];
         },
-
-        //calculates cpm and cpmChange and updates them in appData
+//calculates cpm and cpmChange and updates them in appData
         calculateCpm: function(){
-            var cpmOld = appData.results.cpm;
-            var numOfCorrectCharacters = appData.results.numOfCorrectCharacters;
+            let cpmOld = appData.results.cpm;
+            let numOfCorrectCharacters = appData.results.correctChars;
             if(appData.indicators.timeLeft != appData.indicators.totalTestTime){
                 appData.results.cpm = Math.round(60 * numOfCorrectCharacters/(appData.indicators.totalTestTime - appData.indicators.timeLeft));
             }else{
                 appData.results.cpm = 0
             }
             appData.results.cpmChange = appData.results.cpm - cpmOld;
-
             return [appData.results.cpm, appData.results.cpmChange];
         },
-
-        //calculates accuracy and accuracyChange and updates them in appData
+//calculates accuracy and accuracyChange and updates them in appData
         calculateAccuracy: function(){
-            var accuracyOld = appData.results.accuracy;
-            var numOfCorrectCharacters = appData.results.numOfCorrectCharacters;
-            var numOfTestCharacters = appData.results.numOfTestCharacters;
-
+            let accuracyOld = appData.results.accuracy;
+            let numOfCorrectCharacters = appData.results.correctChars;
+            let numOfTestCharacters = appData.results.TestChars;
             if(appData.indicators.timeLeft != appData.indicators.totalTestTime){
                 if(numOfTestCharacters != 0){
                     appData.results.accuracy = Math.round(100 * numOfCorrectCharacters/numOfTestCharacters);
@@ -157,62 +141,42 @@ var dataModule = (function(){
             appData.results.accuracyChange = appData.results.accuracy - accuracyOld;
 
             return [appData.results.accuracy, appData.results.accuracyChange];
-
         },
 
-    //test words
-
-        // fills words.testWords
+    //TEXTS PROVIDER
+// fills words.testWords
         fillListOfTestWords: function(textNumber, words){
-            var result = words.split(" ");
-
-            if(textNumber == 0){
-                //shuffle words
-                result = shuffle(result);
-                //capitalise random strings
-                result = capitalizeRandom(result);
-                //add a random punctuation
-                result = addRandomPunctuation(result);
-            }
-
-
+            let result = words.split(" ");
             appData.words.testWords = result;
         },
-
-        // get list of test words: words.testWords
+// get list of test words: words.testWords
         getListofTestWords: function(){
             return appData.words.testWords;
         },
-
-        // increments the currentWordIndex - updates the current word (appData.words.currentWord) by creating a new instance of the word class - updates numOfCorrectWords, numOfCorrectCharacters and numOfTestCharacters
+// increments the currentWordIndex - updates the current word (appData.words.currentWord) by creating a new instance of the word class - updates numOfCorrectWords, numOfCorrectCharacters and numOfTestCharacters
         moveToNewWord: function(){
             if(appData.words.currentWordIndex > -1){
-
-                //update the number of correct words
+//update the number of correct words
                 if(appData.words.currentWord.value.isCorrect == true){
-                    appData.results.numOfCorrectWords ++;
+                    appData.results.correctWords ++;
                 }
-
-                //update number of correct characters
-                appData.results.numOfCorrectCharacters += appData.words.currentWord.characters.totalCorrect;
-
-                //update number of test characters
-                appData.results.numOfTestCharacters += appData.words.currentWord.characters.totalTest;
+//update number of correct characters
+                appData.results.correctChars += appData.words.currentWord.characters.totalCorrect;
+//update number of test characters
+                appData.results.testChars += appData.words.currentWord.characters.totalTest;
             }
             appData.words.currentWordIndex ++;
-            var currentIndex = appData.words.currentWordIndex;
-            var newWord = new word(currentIndex);
+            let currentIndex = appData.words.currentWordIndex;
+            let newWord = new word(currentIndex);
             appData.words.currentWord = newWord;
         },
-
-        //get the current word index
+//get the current word index
         getCurrentWordIndex(){
             return appData.words.currentWordIndex;
         },
-
-        //get current word
+//get current word
         getCurrentWord(){
-            var currentWord = appData.words.currentWord;
+            let currentWord = appData.words.currentWord;
             return {
                 value: {
                     correct: currentWord.value.correct,
@@ -220,15 +184,12 @@ var dataModule = (function(){
                 }
             };
         },
-
-        // updates current word using user input
+// updates current word using user input
         updateCurrentWord: function(value){
             appData.words.currentWord.update(value);
         },
         returnData(){
             console.log(appData);
         }
-
     }
-
 })();
