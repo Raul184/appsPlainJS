76206840c53aa56f5ad93948bcd4a6fbea4fad57
RrChaos <rrchaos@tutanota.com>
2018-12-11 17:39:56 +0000

    ExpensesAppES6

--- ExpensesAppES6/js/app.js
+++ ExpensesAppES6/js/app.js
@@ -1,21 +1,40 @@
 // DATA Module
-class Presuspuesto{
-  constructor(presupuesto){
-    this.presupuesto = Number(presupuesto);
-    this.restante = Number(presupuesto); //for method
-  }                   //default
-  balance(amount = 0){
-    return this.restante -= Number(cantidad)
+class Presupuesto{
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto); //for method
+      }                   //default
+    balance(amount = 0){
+        return this.restante -= Number(cantidad);
+    }
+}
+
+class Interfaz{
+  insertAmount(amount){
+    // Locate
+    const presupuesto = document.getElementById('total');
+    const restante = document.getElementById('restante');
+    // Show on UI
+    presupuesto.innerHTML = `${amount}`;
+    restante.innerHTML = `${amount}`;
   }
 }
+
 
 
 // UI Module
 const UI = (function(){
   const budget = prompt('Please let me know about your weekly budget');
+  const DOM = {
+    amount: document.getElementById('cantidad'),
+    agrega: document.querySelector('.btn-primary')
+  }
   return{
     getBudget: function(){
       return budget;
+    },
+    getDom: function(){
+      return DOM;
     }
 
   }//R ends
@@ -24,16 +43,25 @@ const UI = (function(){
 
 // Events Listeners Module
 const EVI = (function(){
-  let budgetEv = UI.getBudget();
+  let budgetE = UI.getBudget();
+  //dom
+  let domi = UI.getDom();
   return{
     init: function(){
+// SET BUDGET
       document.addEventListener("DOMContentLoaded", function(){
-        if(budgetEv === null || budgetEv === '' || typeof budgetEv !== 'number'){
+        if(budgetE === null || budgetE === '' ){
           window.location.reload(); //reload if User does not input budget
-        } else{ //process input
+        } else{ //process input (budget)
           console.log('agregado');
+          const budgetCheck = new Presupuesto(budgetE); //instantiate budget C
+          const interface = new Interfaz(); //instantiate UI C
+          // use its method now
+          interface.insertAmount(budgetCheck.presupuesto);
         }
       });
+// AGREGGATE EXPENSES
+      domi.agrega.addEventListener('click', aggregates);
     }
   } //R ends
 })();
