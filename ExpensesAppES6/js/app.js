0fb1c14001787b396e722b534ed58b19670a076a
RrChaos <rrchaos@tutanota.com>
2018-12-12 00:31:40 +0000

    ExpensesAppES6

--- ExpensesAppES6/js/app.js
+++ ExpensesAppES6/js/app.js
@@ -1,139 +1,126 @@
-// DATA Module
-class Presupuesto{
-    constructor(presupuesto){
-        this.presupuesto = Number(presupuesto);
-        this.restante = Number(presupuesto); //for method
-      }                   //default
-    balance(cantidad = 0){
-        return this.restante -= Number(cantidad);
-    }
-}
-//
+// Variables
+const presupuestoUsuario = prompt('Cual es tu presupuesto Semanal?');
+const formulario = document.getElementById('agregar-gasto');
 let cantidadPresupuesto;
 
-class Interfaz{
-// INPUT SAVER
-  insertAmount(amount){
-    // Locate
-    const presupuesto = document.getElementById('total');
-    const restante = document.getElementById('restante');
-    // Show on UI
-    presupuesto.innerHTML = `${amount}`;
-    restante.innerHTML = `${amount}`;
-  }
-// INPUT SAVED MESSAGE
-  showMessage(m , t){
-    // Dom
-    const dom = UI.getDom();
-    //html Injection
-    const divMessag = document.createElement('div');
-    // class & styles
-    if(t === 'error'){
-      divMessag.classList.add('error-message');
-    } else if(t === 'cool'){
-      divMessag.classList.add('success-message');
-    }
-    //add Message
-    divMessag.appendChild(document.createTextNode(m))
-    // location
-    dom.primary.insertBefore(divMessag, dom.form);
-    setTimeout(function(){
-      divMessag.remove();
-    }, 1500);
-  }
-// EXPENSES ADDED ON UI
-  attachExpense(name, number){
-    //location to insert
-    const list = document.querySelector('.list-group');
-    // html element
-    const li = document.createElement('li');
-    li.className = 'expAccounted';
-    //html content
-    li.innerHTML = `
-          ${name}
-          $${number}
-    `;
-    console.log(li);
-    // html injection
-    list.appendChild(li);
-  }
-// CALCULATIONS ON BUDGET
-    balance(cantidad){
-      const restante = document.querySelector('#restante');
-      const budgetLeft = cantidadPresupuesto.balance(cantidad);
-      restante.innerHTML = `${budgetLeft}`;
-      console.log(presupuesto);
-    }
+
+// Clases
+// Clase de Presupuesto
+class Presupuesto {
+     constructor(presupuesto) {
+          this.presupuesto = Number(presupuesto);
+          this.restante = Number(presupuesto);
+     }
+     // Método para ir restando del presupuesto actual
+     presupuestoRestante(cantidad = 0) {
+          return this.restante -= Number(cantidad);
+     }
+}
+// Clase de Interfaz maneja todo lo relacionado a el HTML
+
+class Interfaz {
+     insertarPresupuesto(cantidad) {
+          const presupuestoSpan = document.querySelector('span#total');
+          const restanteSpan = document.querySelector('span#restante');
+
+          // Insertar al HTML
+          presupuestoSpan.innerHTML = `${cantidad}`;
+          restanteSpan.innerHTML = `${cantidad}`;
+     }
+     imprimirMensaje(mensaje, tipo) {
+          const divMensaje = document.createElement('div');
+          divMensaje.classList.add('text-center', 'alert');
+          if(tipo === 'error') {
+               divMensaje.classList.add('alert-danger');
+          } else {
+               divMensaje.classList.add('alert-success');
+          }
+          divMensaje.appendChild(document.createTextNode(mensaje));
+          // Insertar en el DOM
+          document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+          // Quitar el alert despues de 3 segundos
+          setTimeout(function() {
+               document.querySelector('.primario .alert').remove();
+               formulario.reset();
+          }, 3000);
+     }
+     // Inserta los gastos a la lista 
+     agregarGastoListado(nombre, cantidad) {
+          const gastosListado = document.querySelector('#gastos ul');
+
+          // Crear un LI
+          const li = document.createElement('li');
+          li.className = 'list-group-item d-flex justify-content-between align-items-center';
+          // Insertar el gasto
+          li.innerHTML = `
+               ${nombre}
+               <span class="badge badge-primary badge-pill"> $ ${cantidad} </span>
+          `;
+
+          // Insertar al HTML
+          gastosListado.appendChild(li);
+     }
+     // Comprueba el presupuesto restante
+     presupuestoRestante(cantidad) {
+          const restante = document.querySelector('span#restante');
+          // Leemos el presupuesto restante
+          const presupuestoRestanteUsuario =  cantidadPresupuesto.presupuestoRestante(cantidad);
+
+          restante.innerHTML = `${presupuestoRestanteUsuario}`;
+
+          this.comprobarPresupuesto();
+     }
+     // Cambia de color el presupuesto restante
+     comprobarPresupuesto() {
+          const presupuestoTotal = cantidadPresupuesto.presupuesto;
+          const presupuestoRestante = cantidadPresupuesto.restante;
+
+          // Comprobar el 25% 
+          if( (presupuestoTotal / 4) > presupuestoRestante) {
+               const restante = document.querySelector('.restante');
+               restante.classList.remove('alert-success', 'alert-warning');
+               restante.classList.add('alert-danger');
+          } else if( (presupuestoTotal / 2) > presupuestoRestante) {
+               const restante = document.querySelector('.restante');
+               restante.classList.remove('alert-success');
+               restante.classList.add('alert-warning');
+          }
+     }
 }
 
 
-// UI Module
-const UI = (function(){
-  const budget = prompt('Please let me know about your weekly budget');
-  const DOM = {
-    primary: document.querySelector('.primario'),
-    form: document.getElementById('agregar-gasto'),
-    expense: document.getElementById('gasto'),
-    amountExp: document.getElementById('cantidad'),
-    restante: document.getElementById('restante'),
-    agrega: document.querySelector('.btn-primary')
-  }
-  return{
-    getBudget: function(){
-      return budget;
-    },
-    getDom: function(){
-      return DOM;
-    }
 
-  }//R ends
-})();
+// Event Listeners
+document.addEventListener('DOMContentLoaded', function() {
+     if(presupuestoUsuario === null || presupuestoUsuario === '') {
+          window.location.reload();
+     } else {
+          // Instanciar un presupuesto
+          cantidadPresupuesto = new Presupuesto(presupuestoUsuario);
+          // Instanciar la clase de Interfaz
+          const ui = new Interfaz();
+          ui.insertarPresupuesto(cantidadPresupuesto.presupuesto);
+          
+     }
+});
 
+formulario.addEventListener('submit', function(e) {
+     e.preventDefault();
+     
+     // Leer del formulario de Gastos
+     const nombreGasto = document.querySelector('#gasto').value;
+     const cantidadGasto = document.querySelector('#cantidad').value;
 
-// Events Listeners Module
-const EVI = (function(){
-  let budgetE = UI.getBudget();
-  //dom
-  let domi = UI.getDom();
-  return{
-    init: function(){
-// SET BUDGET
-      document.addEventListener("DOMContentLoaded", function(){
-        if(budgetE === null || budgetE === '' ){
-          window.location.reload(); //reload if User does not input budget
-        } else{ //process input (budget)
-          console.log('agregado');
-          const budgetCheck = new Presupuesto(budgetE); //instantiate budget C
-          const interface = new Interfaz(); //instantiate UI C
-          // use its method now
-          interface.insertAmount(budgetCheck.presupuesto);
-        }
-      });
-// FORM complete?
-      domi.form.addEventListener('submit', function(e){
-        e.preventDefault();
-        const interface = new Interfaz(); //instantiate UI C
-        let expDescrip = domi.expense.value;
-        let expAmount = domi.amountExp.value
-        if( expDescrip === '' || expAmount === ''){
-          interface.showMessage('Please check your input', 'error'); //error
-          expDescrip = '' ;
-          expAmount = '' ;
-        }else{
-          //show on ui
-          interface.showMessage('Accounted ', 'cool'); //success
-          // set it on budget UI
-          interface.attachExpense(expDescrip, expAmount);
-          expDescrip = '' ;
-          expAmount = '' ;
-          //calculations
-          const budgetCheck = new Presupuesto(budgetE); //instantiate budget
-          budgetCheck.balance(expAmount);
-          interface.balance(expAmount);
-        }
-        console.log('enviado');
-      })
-    }//init
-  } //R ends
-})();
-EVI.init();
+     // Instanciar la Interfaz
+     const ui = new Interfaz();
+
+     // Comprobar que los campos no esten vacios
+     if(nombreGasto === '' || cantidadGasto === '') {
+          // 2 parametros: mensaje y tipo
+          ui.imprimirMensaje('Hubo un error', 'error');
+     } else {
+          // Insertar en el HTML
+          ui.imprimirMensaje('Correcto', 'correcto');
+          ui.agregarGastoListado(nombreGasto, cantidadGasto);
+          ui.presupuestoRestante(cantidadGasto);
