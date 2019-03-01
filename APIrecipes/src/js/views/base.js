b2ce2bf1abc43a73802e3564ec267c497b6d3256
RrChaos <rrchaos@tutanota.com>
2019-03-01 13:51:33 +0000

    SearchView Added

--- APIrecipes/src/js/views/base.js
+++ APIrecipes/src/js/views/base.js
@@ -4,7 +4,7 @@ export const elements = {
       searchInput: document.querySelector('.search__field'),
       resultsFrame: document.querySelector('.results'),
       leftResults: document.querySelector('.results__list'),
-      resultPages: document.querySelector('.result__pages')
+      resultPages: document.querySelector('.results__pages')
 };
 
 //GIF-Loader
@@ -23,4 +23,31 @@ export const loaderGif = parent => {
       },1700);
 };
 
+//SEARCHVIEW HELPERS
+ //2. HELPER for inputRender >> buttons on Pagination
+ //Pagination buttons maker
+const maker = (page, type) => `
+      <button class="btn-inline results__btn--${type}" data-id=${type === 'prev' ? page - 1: page + 1}>
+                  <svg class="search__icon">
+                        <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+                  </svg>
+                  <span>Page ${type === 'prev' ? page - 1: page + 1}</span>
+            </button>
+`;
+ //2. HELPER for Buttons on Pagination
+export const renderButtons = (page, numResults, resPpage) => {
+       const pages = Math.ceil(numResults / resPpage);
+       let button;
+       if (page === 1 && pages > 1) {
+             //next
+             button = maker(page, 'next');
+       } else if (page < pages) {
+             //next & previous
+             button = `${maker(page, 'prev')} ${maker(page, 'next')}`;
+       } else if (page === pages && pages > 1) {
+             //previous
+             button = maker(page, 'prev');
+       }
 
+       elements.resultPages.insertAdjacentHTML('afterbegin', button);
+ };
--- APIrecipes/src/js/views/searchView.js
+++ APIrecipes/src/js/views/searchView.js
@@ -1,27 +1,17 @@
 //DOM
-import {elements as DOM} from './base';
+import {elements as DOM, renderButtons} from './base';
+
+
 
 //1.Input getter
  export const getInput = () => DOM.searchInput.value;
 
 
  //-- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- --
- //2. Input render 
- export const inputRender = (recipes, page=1, resPerpage=10) => {
-       //pagination
-       const start = (page - 1) * resPerpage;
-       console.log(start);
-       const end = (page * resPerpage);
-       console.log(end);
-       //results
-       recipes.slice(start, end).forEach(renderRecipes);
-      //render buttons
-      //renderButtons(page, recipes.length, resPerpage);
- };
 
  //2.HELPER for inputRender
- const renderRecipes = (recipe) =>{
-      const markUp = `<li>
+ const renderRecipes = (recipe) => {
+       const markUp = `<li>
                     <a class="results__link results__link--active" href="#${recipe.recipe_id}">
                         <figure class="results__fig">
                             <img src="${recipe.image_url}" alt="${recipe.title}">
@@ -32,44 +22,20 @@ import {elements as DOM} from './base';
                         </div>
                     </a>
                 </li>`;
-      DOM.leftResults.insertAdjacentHTML('beforeend', markUp);
- }; 
- 
- //2. HELPER for inputRender >> buttons on Pagination
- //Pagination buttons maker
- const maker = (page, type) => {
-      return `<button class="btn-inline results__btn--${type}" data-id=${type === 'prev' ? page - 1: page + 1}>
-                  <svg class="search__icon">
-                        <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
-                  </svg>
-                  <span>${type === 'prev' ? page - 1: page + 1}</span>
-            </button>
-      `;
-}
-  
- //2. HELPER for Buttons on Pagination
- const renderButtons = (page, numResults, resPpage) => {
-       const pages = Math.ceil(numResults / resPpage);
-       
-       let button;
-       if (page === 1 && pages > 1) {
-             //next
-             button = maker(page, 'next');
-       } else if (page < pages) {
-             //next & previous
-             button = `
-            ${maker(page, 'prev')}
-            ${maker(page, 'next')}
-        `;
-       } else if (page === pages && pages > 1) {
-             //previous
-             button = maker(page, 'prev');
-       }
-
-      DOM.resultPages.insertAdjacentHTML('afterbegin', button);
+       DOM.leftResults.insertAdjacentHTML('beforeend', markUp);
  };
-//-- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- --
+ //2. Input render 
+ export const inputRender = (recipes, page=1, resPerpage=10) => {
+       //pagination
+       const start = (page - 1) * resPerpage;
+       const end = (page * resPerpage);
+       //results
+       recipes.slice(start, end).forEach(renderRecipes);
+      //render buttons
+      renderButtons(page, recipes.length, resPerpage);
+ };
 
+//-- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- --
 
  //3. UI Cleaner for Input
  export const clearInput = () => {
@@ -79,4 +45,5 @@ import {elements as DOM} from './base';
  export const clearList = () => {
        DOM.leftResults.innerHTML = '';
  }
+ 
 //-- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- --
