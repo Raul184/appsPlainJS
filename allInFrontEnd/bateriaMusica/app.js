129a8726016d99a462e483ccaa0dd4474cd6fb23
RrChaos <rrchaos@tutanota.com>
2019-06-19 11:38:50 +0100

    styles added and JS

---
+++ allInFrontEnd/bateriaMusica/app.js
@@ -0,0 +1,28 @@
+const play = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  //rewind to start in case multiple hit
+  audio.currentTime = 0;
+  //play audio tag => By default from 0 to 100. rewind in case of repetition
+  audio.play();
+
+  //key effects
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  key.classList.add("playing");
+  // setTimeout(() => key.classList.remove("playing") , 100);
+}
+
+//best WAY
+function removeTransition(e){
+  if(e.propertyName !== 'transform') return;
+  //key
+  this.classList.remove("playing");
+}
+
+//ON INIT
+(function init(){
+  console.log('init');
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', play);
+})();
---
+++ allInFrontEnd/readme.md
@@ -0,0 +1,6 @@
+Front End Layouts with JS (Next.js) , HTML5 and CSS3
+
+A bunch of stuff well done such as clocks , drums , restaurants desktop orders ....
+
+
+
